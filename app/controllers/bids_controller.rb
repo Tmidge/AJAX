5fe2bfa2a486c9df7ecbd54aa1065ef5affaac9a
Thomas Midgett <tmidge20@gmail.com>
2015-02-25 20:31:28 -0500

    #AJAXED

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,14 +1,18 @@
 class BidsController < ApplicationController
 
   def create
-    @bid = Bid.new(bid_params)
-
-    if @bid.save
-      redirect_to @bid.item, notice: 'Your bid has been recorded!'
-    else
-      @item = @bid.item
-      render "items/show"
-    end
+    @bid = Bid.create(bid_params)
+    #leaving so I can see the origianl fix when the bids create was in the item view
+    
+    # if @bid.save
+    #   respond_to do |i|
+    #     i.html {redirect_to @bid.item, notice: 'Your bid has been recorded!'}
+    #     i.js {render 'items/create.js.erb'}
+    #   end
+    # else
+    #   @item = @bid.item
+    #   render "items/show"
+    # end
   end
 
 
